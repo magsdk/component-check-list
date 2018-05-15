b57eee8b5d7554be86083ac3910f982951feed4a
Harryk13 <stoyan.roman@gmail.com>
2018-05-15 12:25:44 +0300

    fix clearing checked data
    clear checkedData in setData function

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ CheckList.prototype.setData = function ( config ) {
     var index = 0;
 
     List.prototype.setData.call(this, config);
-
+    this.checkedData = [];
     for ( index; index < this.data.length; index++ ) {
         if ( this.data[index].state ) {
             this.checkedData.push(this.data[index]);
